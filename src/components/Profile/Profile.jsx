3051eece6c2d5c0676bf7b83934cae5cfd80ac37
Leonid <valeriy92prog@gmail.com>
2020-04-27 19:02:05 +0300

    Second

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
                 <img
                     src='https://thumbs.dreamstime.com/b/wide-black-brick-wall-texture-dark-gray-brickwork-background-wide-black-brick-wall-texture-spacious-dark-gray-masonry-background-124596974.jpg'/>
