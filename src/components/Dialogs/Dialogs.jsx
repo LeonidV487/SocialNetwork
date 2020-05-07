a13acb834aebbcbeabc98da5a638715dba05433b
Leonid <valeriy92prog@gmail.com>
2020-05-07 13:45:26 +0300

    Third

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import classes from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogMenu}>
+            <div className={classes.dialogs}>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/1">Leonid</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/2">Valeriy</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/3">Artemiy</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/4">Anastasia</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/5">George</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/6">Allen</NavLink>
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How r u?</div>
+                <div className={classes.message}>I am waiting for you</div>
+                <div className={classes.message}>See you soon</div>
+                <div className={classes.message}>Have a nice day!</div>
+                <div className={classes.message}>lmao</div>
+            </div>
         </div>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={classes.header}>
             <img src='https://www.freelogodesign.org/Content/img/logo-samples/flooop.png'/>
